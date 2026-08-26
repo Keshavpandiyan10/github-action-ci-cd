3daec61bfb1bb5451fe039e0e56fac38eb501754
Keshavpandiyan10 <99722023+Keshavpandiyan10@users.noreply.github.com>
2026-08-26 18:33:22 +0530

    Update message for GitHub Action status

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Github Action running"});
+  res.status(200).json({ status: "ok", message: "Github Action running successfully"});
 });
 
 app.get("/health", (req, res) => {
